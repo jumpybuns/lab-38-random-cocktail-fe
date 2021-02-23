7eb14e99dc5d9b65146c7811b398ff79b2e6048e
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-23 11:02:57 -0800

    add App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CocktailPage from '../../containers/CocktailPage';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(routerProps) => <CocktailPage {...routerProps} />}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
